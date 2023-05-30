4174b615b2c380d035daaa412f673bfdcea07a8f
gabriel.leroux@edu.esiee.fr <gabriel.leroux@edu.esiee.fr>
2023-05-30 14:58:14 +0200

    Gère eslint qui faisait tout souligner en jaune et rajoute jusq'au step 3 pour la page healthgoals

--- src/screens/HealthScreen.js
+++ src/screens/HealthScreen.js
@@ -1,8 +1,6 @@
-import React, { useState } from 'react';
-import { View, Text, TextInput, Button, StyleSheet } from 'react-native';
 import { Picker } from '@react-native-picker/picker';
-
-
+import React, { useState } from 'react';
+import { View, TextInput, Button, StyleSheet } from 'react-native';
 
 const HealthScreen = () => {
   const [age, setAge] = useState('');
@@ -22,6 +20,19 @@ const HealthScreen = () => {
     console.log('Weight:', weight);
     console.log('Activity Level:', activityLevel);
     console.log('Health Goal:', healthGoal);
+    const bmr = calculateBMR(age, gender, height, weight);
+    console.log('BMR : ', bmr);
+  };
+
+  const calculateBMR = (age, gender, height, weight) => {
+    let bmr = 0;
+    if (gender === 'male') {
+      bmr = 88.362 + 13.397 * weight + 4.799 * height - 5.677 * age;
+    } else if (gender === 'female') {
+      bmr = 447.593 + 9.247 * weight + 3.098 * height - 4.33 * age;
+    }
+    // To have only 2 decimals
+    return bmr.toFixed(2);
   };
 
   return (
@@ -37,8 +48,7 @@ const HealthScreen = () => {
       <Picker
         style={styles.input}
         selectedValue={gender}
-        onValueChange={(value) => setGender(value)}
-      >
+        onValueChange={(value) => setGender(value)}>
         <Picker.Item label="Select Gender" value="" />
         <Picker.Item label="Male" value="male" />
         <Picker.Item label="Female" value="female" />
@@ -63,8 +73,7 @@ const HealthScreen = () => {
       <Picker
         style={styles.input}
         selectedValue={activityLevel}
-        onValueChange={(value) => setActivityLevel(value)}
-      >
+        onValueChange={(value) => setActivityLevel(value)}>
         <Picker.Item label="Select Activity Level" value="" />
         <Picker.Item label="Sedentary" value="sedentary" />
         <Picker.Item label="Light Exercise" value="light-exercise" />
@@ -76,15 +85,18 @@ const HealthScreen = () => {
       <Picker
         style={styles.input}
         selectedValue={healthGoal}
-        onValueChange={(value) => setHealthGoal(value)}
-      >
+        onValueChange={(value) => setHealthGoal(value)}>
         <Picker.Item label="Select Health Goal" value="" />
         <Picker.Item label="Weight Loss" value="weight-loss" />
         <Picker.Item label="Weight Maintenance" value="weight-maintenance" />
         <Picker.Item label="Weight Gain" value="weight-gain" />
       </Picker>
 
-      <Button title="Submit" onPress={handleSubmit} />
+      <Button
+        title="Submit"
+        disabled={!age || !gender || !height || !weight || !activityLevel || !healthGoal}
+        onPress={handleSubmit}
+      />
     </View>
   );
 };
