ea82033a8c2451b28139cbb74fdb623511e7edb5
gabriel.leroux@edu.esiee.fr <gabriel.leroux@edu.esiee.fr>
2023-06-03 18:22:13 +0200

    Step 6 & 7 fais, à tester à l'esiee et à régler le CSS en fonction. CSS crée à partir de ce que je pense et  partir des CSS que j'ai utilisé précedemment pour mon bigot

--- src/screens/FoodDatabase.js
+++ src/screens/FoodDatabase.js
@@ -1,10 +1,19 @@
+import { Picker } from '@react-native-picker/picker';
 import React, { useState } from 'react';
-import { Text, View, TextInput, Button, StyleSheet } from 'react-native';
+import { Text, View, TextInput, Button, StyleSheet, Modal } from 'react-native';
 
 const FoodDatabase = () => {
   const [searchQuery, setSearchQuery] = useState('');
   const [foodData, setFoodData] = useState(null);
   const [error, setError] = useState(null);
+  const [selectedMeal, setSelectedMeal] = useState('');
+  const [modalVisible, setModalVisible] = useState(false);
+  const [mealPlan, setMealPlan] = useState({
+    Breakfast: [],
+    Lunch: [],
+    Snack: [],
+    Dinner: [],
+  });
 
   const APP_ID = '44e2f578';
   const APP_KEY = 'b91b52c750f2618779c8da62c2ae355a';
@@ -32,6 +41,21 @@ const FoodDatabase = () => {
       }
     }
   };
+  const handleAddToMealPlan = () => {
+    if (selectedMeal.trim() !== '') {
+      // Add
+      const updatedMealPlan = { ...mealPlan };
+      updatedMealPlan[selectedMeal].push(foodData);
+
+      setMealPlan(updatedMealPlan);
+
+      // Reset state variables
+      setFoodData(null);
+      setSelectedMeal('');
+      setModalVisible(false);
+    }
+  };
+
   return (
     <View style={styles.container}>
       <TextInput
@@ -44,18 +68,36 @@ const FoodDatabase = () => {
       {error && <Text style={styles.error}>{error}</Text>}
       {foodData && (
         <View style={styles.resultContainer}>
-          <Text style={styles.label}>Food Name:</Text>
+          <Text style={styles.label}>Food Name : </Text>
           <Text style={styles.value}>{foodData.label}</Text>
-          <Text style={styles.label}>Calories:</Text>
+          <Text style={styles.label}>Calories : </Text>
           <Text style={styles.value}>{foodData.calories.toFixed(2)}</Text>
-          <Text style={styles.label}>Other Nutritional Facts:</Text>
+          <Text style={styles.label}>Other Nutritional Facts : </Text>
           {foodData.digest.map((item, index) => (
             <Text key={index} style={styles.value}>
               {item.label}: {item.total.toFixed(2)} {item.unit}
             </Text>
           ))}
+          <Button title="Add to Meal Plan" onPress={() => setModalVisible(true)} />
         </View>
       )}
+       <Modal visible={modalVisible} animationType="slide">
+        <View style={styles.modalContainer}>
+          <Text style={styles.modalTitle}>Select Meal Type : </Text>
+          <Picker
+            style={styles.picker}
+            selectedValue={selectedMeal}
+            onValueChange={(value) => setSelectedMeal(value)}>
+            <Picker.Item label="Select Meal" value="" />
+            <Picker.Item label="Breakfast" value="Breakfast" />
+            <Picker.Item label="Lunch" value="Lunch" />
+            <Picker.Item label="Dinner" value="Dinner" />
+            <Picker.Item label="Snack" value="Snack" />
+          </Picker>
+          <Button title="Add to Meal Plan" onPress={handleAddToMealPlan} />
+          <Button title="Cancel" onPress={() => setModalVisible(false)} />
+        </View>
+      </Modal>
     </View>
   );
 };
@@ -88,6 +130,19 @@ const styles = StyleSheet.create({
   error: {
     color: 'red',
     marginTop: 8,
+  },
+  modalContainer: {
+    flex: 1,
+    padding: 16,
+    justifyContent: 'center',
+  },
+  modalTitle: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginBottom: 16,
+  },
+  picker: {
+    marginBottom: 16,
   },
 });
 
