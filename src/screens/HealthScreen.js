ebb076a4abe731699fb5d2d899d5c81d544308bc
Hug0H1 <hugo.henriques@edu.esiee.fr>
2023-06-17 15:55:52 +0200

    Ajout scroll View pour compatibilite IOS sur le health screen + LF replaced by CRLF sur les screens

--- src/screens/HealthScreen.js
+++ src/screens/HealthScreen.js
@@ -1,6 +1,6 @@
 import { Picker } from '@react-native-picker/picker';
 import React, { useState } from 'react';
-import { View, TextInput, Button, StyleSheet } from 'react-native';
+import { View, TextInput, Button, StyleSheet, ScrollView } from 'react-native';
 
 import ResultScreen from './ResultScreen';
 
@@ -80,70 +80,72 @@ const HealthScreen = () => {
 
   return (
     <View style={styles.container}>
-      <TextInput
-        style={styles.input}
-        placeholder="Age"
-        maxLength={2}
-        value={age}
-        onChangeText={setAge}
-        keyboardType="numeric"
-      />
+      <ScrollView>
+        <TextInput
+          style={styles.input}
+          placeholder="Age"
+          maxLength={2}
+          value={age}
+          onChangeText={setAge}
+          keyboardType="numeric"
+        />
 
-      <Picker
-        style={styles.input}
-        selectedValue={gender}
-        onValueChange={(value) => setGender(value)}>
-        <Picker.Item label="Select Gender" value="" />
-        <Picker.Item label="Male" value="male" />
-        <Picker.Item label="Female" value="female" />
-      </Picker>
+        <Picker
+          style={styles.input}
+          selectedValue={gender}
+          onValueChange={(value) => setGender(value)}>
+          <Picker.Item label="Select Gender" value="" />
+          <Picker.Item label="Male" value="male" />
+          <Picker.Item label="Female" value="female" />
+        </Picker>
 
-      <TextInput
-        style={styles.input}
-        placeholder="Height (in cm)"
-        maxLength={3}
-        value={height}
-        onChangeText={setHeight}
-        keyboardType="numeric"
-      />
+        <TextInput
+          style={styles.input}
+          placeholder="Height (in cm)"
+          maxLength={3}
+          value={height}
+          onChangeText={setHeight}
+          keyboardType="numeric"
+        />
 
-      <TextInput
-        style={styles.input}
-        placeholder="Weight (in kg)"
-        maxLength={3}
-        value={weight}
-        onChangeText={setWeight}
-        keyboardType="numeric"
-      />
+        <TextInput
+          style={styles.input}
+          placeholder="Weight (in kg)"
+          maxLength={3}
+          value={weight}
+          onChangeText={setWeight}
+          keyboardType="numeric"
+        />
 
-      <Picker
-        style={styles.input}
-        selectedValue={activityLevel}
-        onValueChange={(value) => setActivityLevel(value)}>
-        <Picker.Item label="Select Activity Level" value="" />
-        <Picker.Item label="Sedentary" value="sedentary" />
-        <Picker.Item label="Light Exercise" value="light-exercise" />
-        <Picker.Item label="Moderate Exercise" value="moderate-exercise" />
-        <Picker.Item label="Heavy Exercise" value="heavy-exercise" />
-        <Picker.Item label="Extra Active" value="extra-active" />
-      </Picker>
+        <Picker
+          style={styles.input}
+          selectedValue={activityLevel}
+          onValueChange={(value) => setActivityLevel(value)}>
+          <Picker.Item label="Select Activity Level" value="" />
+          <Picker.Item label="Sedentary" value="sedentary" />
+          <Picker.Item label="Light Exercise" value="light-exercise" />
+          <Picker.Item label="Moderate Exercise" value="moderate-exercise" />
+          <Picker.Item label="Heavy Exercise" value="heavy-exercise" />
+          <Picker.Item label="Extra Active" value="extra-active" />
+        </Picker>
 
-      <Picker
-        style={styles.input}
-        selectedValue={healthGoal}
-        onValueChange={(value) => setHealthGoal(value)}>
-        <Picker.Item label="Select Health Goal" value="" />
-        <Picker.Item label="Weight Loss" value="weight-loss" />
-        <Picker.Item label="Weight Maintenance" value="weight-maintenance" />
-        <Picker.Item label="Weight Gain" value="weight-gain" />
-      </Picker>
+        <Picker
+          style={styles.input}
+          selectedValue={healthGoal}
+          onValueChange={(value) => setHealthGoal(value)}>
+          <Picker.Item label="Select Health Goal" value="" />
+          <Picker.Item label="Weight Loss" value="weight-loss" />
+          <Picker.Item label="Weight Maintenance" value="weight-maintenance" />
+          <Picker.Item label="Weight Gain" value="weight-gain" />
+        </Picker>
 
-      <Button
-        title="Submit"
-        disabled={!age || !gender || !height || !weight || !activityLevel || !healthGoal}
-        onPress={handleSubmit}
-      />
-      {showResult && <ResultScreen totalCalories={totalCalories} />}
+        <Button
+          title="Submit"
+          disabled={!age || !gender || !height || !weight || !activityLevel || !healthGoal}
+          onPress={handleSubmit}
+        />
+        {showResult && <ResultScreen totalCalories={totalCalories} />}
+      </ScrollView>
     </View>
   );
 };
