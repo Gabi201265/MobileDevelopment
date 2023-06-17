784b207aff84d7c4277bb48a79f61ed61c73ce14
Hug0H1 <hugo.henriques@edu.esiee.fr>
2023-06-17 15:06:08 +0200

    rebase origin

--- App.js
+++ App.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 import AppNavigator from './src/navigation/AppNavigator';
 
 const App = () => {
+<<<<<<< HEAD
   return <AppNavigator />;
+=======
+  return (
+    <SafeAreaProvider>
+      <NavigationContainer>
+        <AppNavigator />
+      </NavigationContainer>
+    </SafeAreaProvider>
+  );
+>>>>>>> a9e8440 (vue/coherence ios)
 };
 
 export default App;
--- src/navigation/AppNavigator.js
+++ src/navigation/AppNavigator.js
@@ -1,5 +1,8 @@
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+<<<<<<< HEAD
 import { NavigationContainer } from '@react-navigation/native';
+=======
+>>>>>>> a9e8440 (vue/coherence ios)
 import React from 'react';
 import Icon from 'react-native-vector-icons/FontAwesome';
 
@@ -12,6 +15,7 @@ const Tab = createBottomTabNavigator();
 
 const AppNavigator = () => {
   return (
+<<<<<<< HEAD
     <MealPlanProvider>
       <NavigationContainer>
         <Tab.Navigator>
@@ -39,6 +43,31 @@ const AppNavigator = () => {
         </Tab.Navigator>
       </NavigationContainer>
     </MealPlanProvider>
+=======
+    <Tab.Navigator>
+      <Tab.Screen
+        name="Health Goals"
+        component={HealthScreen}
+        options={{
+          tabBarIcon: ({ color, size }) => <Icon name="heart" color={color} size={size} />,
+        }}
+      />
+      <Tab.Screen
+        name="Food Database"
+        component={FoodDatabase}
+        options={{
+          tabBarIcon: ({ color, size }) => <Icon name="cutlery" color={color} size={size} />,
+        }}
+      />
+      <Tab.Screen
+        name="Meal Planning"
+        component={MealPlanning}
+        options={{
+          tabBarIcon: ({ color, size }) => <Icon name="calendar" color={color} size={size} />,
+        }}
+      />
+    </Tab.Navigator>
+>>>>>>> a9e8440 (vue/coherence ios)
   );
 };
 
